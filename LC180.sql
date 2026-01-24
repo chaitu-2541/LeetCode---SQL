0540789d5a1ce590f0ab77f1c87a92d0ec07012b
Chaitanya-Dev <76583086+chaitu-2541@users.noreply.github.com>
2026-01-24 23:11:54 +0530

    Consecutive Numbers

---
+++ LC180.sql
@@ -0,0 +1,19 @@
+
+--Problem: LC180
+--Description: Consecutive Numbers
+-- Link : https://leetcode.com/problems/consecutive-numbers/description/
+
+
+
+
+-- LC180.sql
+SELECT DISTINCT l1.num as ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.id = l2.id-1
+    AND l2.id = l3.id-1
+    AND l1.num = l2.num
+    AND l2.num = l3.num
